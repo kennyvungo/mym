e893874fe4d8da7296129d55e87ef7e11c5b9cd7
Kenny <kennyvungo@gmail.com>
2023-10-06 13:56:56 -0700

    set up react front end

---
+++ frontend/src/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import { Provider } from 'react-redux';
+import {BrowserRouter} from 'react-router-dom';
+import configureStore from './store/store'
+let store = configureStore({});
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+function Root(){
+  return(
+    <Provider store={store} >
+      <BrowserRouter>
+        <App/>
+      </BrowserRouter>
+    </Provider>
+  )
+}
+
+root.render(
+  <React.StrictMode>
+    <Root />
+  </React.StrictMode>
+);
+
