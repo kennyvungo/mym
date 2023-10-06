e686006cb75cd5f5d8f2b43d37e71e428b391470
Kenny <kennyvungo@gmail.com>
2023-10-06 13:07:21 -0700

    finished validations

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -6,6 +6,8 @@ const User = mongoose.model('user');
 const passport = require('passport');
 const { loginUser, restoreUser } = require('../../config/passport');
 const { isProduction } = require('../../config/keys');
+const validateRegisterInput = require('../../validations/register');
+const validateLoginInput = require('../../validations/login');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -28,7 +30,7 @@ router.get('/current', restoreUser, (req, res) => {
     firstName: req.user.firstName
   });
 });
-router.post('/register', async (req, res, next) => {
+router.post('/register',validateRegisterInput, async (req, res, next) => {
   const user = await User.findOne({
     username: req.body.username 
   });
@@ -70,7 +72,7 @@ router.post('/register', async (req, res, next) => {
     })
   });
 });
-router.post('/login', async (req, res, next) => {
+router.post('/login',validateLoginInput, async (req, res, next) => {
   passport.authenticate('local', async function(err, user) {
     if (err) return next(err);
     if (!user) {
