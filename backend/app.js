9e62bcc76460060438b2de160946a0445d1d9b0f
Kenny <kennyvungo@gmail.com>
2023-10-05 22:19:22 -0700

    added 404 error on route not found

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-
+const debug = require('debug');
 const cors = require('cors');
 const csurf = require('csurf');
 const { isProduction } = require('./config/keys');
@@ -13,7 +13,22 @@ const usersRouter = require('./routes/api/users');
 const csrfRouter = require('./routes/api/csrf');
 
 const app = express();
-
+app.use((req, res, next) => {
+    const err = new Error('Not Found');
+    err.statusCode = 404;
+    next(err);
+  });
+const serverErrorLogger = debug('backend:error');
+app.use((err, req, res, next) => {
+    serverErrorLogger(err);
+    const statusCode = err.statusCode || 500;
+    res.status(statusCode);
+    res.json({
+      message: err.message,
+      statusCode,
+      errors: err.errors
+    })
+  });
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
