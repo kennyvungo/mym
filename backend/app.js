d61976829e2c73448cbd77e9c35e00edede95c7a
Kenny <kennyvungo@gmail.com>
2023-10-07 18:48:24 -0700

    disabled cors

--- backend/app.js
+++ backend/app.js
@@ -21,16 +21,16 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-if (!isProduction) {
-    app.use(cors());
-}
-app.use(cors(
-    {
-        origin: ["https://mym-client-nine.vercel.app/"],
-        methods:["POST","GET"],
-        credentials: true
-    }
-))
+// if (!isProduction) {
+//     app.use(cors());
+// }
+// app.use(cors(
+//     {
+//         origin: ["https://mym-client-nine.vercel.app/"],
+//         methods:["POST","GET"],
+//         credentials: true
+//     }
+// ))
 app.use(
     csurf({
         cookie: {
