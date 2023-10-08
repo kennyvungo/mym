68d90af71857c04d1cc7ee0603fcfc15e15307e0
Kenny <kennyvungo@gmail.com>
2023-10-07 21:38:48 -0700

    added back in cors

--- backend/app.js
+++ backend/app.js
@@ -21,9 +21,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-if (isProduction) {
-    app.use(cors());
-}
+// if (isProduction) {
+//     app.use(cors());
+// }
+
+app.use(cors());
+
 // app.use(
 //     csurf({
 //         cookie: {
